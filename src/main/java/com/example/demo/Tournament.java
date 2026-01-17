3ab78309d2ba307a46d43c5ac0bd5816507492db
BinBaumiii <dennisgross005@gmail.com>
2026-01-17 17:12:34 +0100

    entity generationValue auf Identity geaendert

--- src/main/java/com/example/demo/Tournament.java
+++ src/main/java/com/example/demo/Tournament.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class Tournament {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String winner;
